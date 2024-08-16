4c636e9478e5aaf429888edd89842ca33566ac63
User <nahidbinwadood@gmail.com>
2024-08-16 17:40:12 +0600

    added dummy products

--- src/Pages/Homepage/Properties/Properties.jsx
+++ src/Pages/Homepage/Properties/Properties.jsx
@@ -1,16 +1,36 @@
- 
+import { useEffect, useState } from "react";
+import ProductsCard from "../../../Components/ProductsCard";
+
 const Properties = () => {
-    return (
-        <div className="mt-20 container mx-auto">
-            <div className="text-center space-y-4">
-                <h2 className="text-4xl font-bold">Explore All Products</h2>
-                <p>Discover our comprehensive selection of premium computer parts tailored for every gamer’s needs. From powerful processors to high-performance graphics cards and everything in between, GameLand offers a one-stop shop for building, upgrading, or customizing your gaming rig. Dive into our collection and find the perfect components to power up your next gaming adventure.</p>
-            </div>
-            <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                
-            </div>
-        </div>
-    );
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  console.log(data);
+  return (
+    <div className="mt-20 container mx-auto">
+      <div className="text-center space-y-4">
+        <h2 className="text-4xl font-bold">Explore All Products</h2>
+        <p>
+          Discover our comprehensive selection of premium computer parts
+          tailored for every gamer’s needs. From powerful processors to
+          high-performance graphics cards and everything in between, GameLand
+          offers a one-stop shop for building, upgrading, or customizing your
+          gaming rig. Dive into our collection and find the perfect components
+          to power up your next gaming adventure.{" "}
+        </p>
+      </div>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data.map((product, idx) => (
+          <ProductsCard key={idx} product={product}></ProductsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Properties;
